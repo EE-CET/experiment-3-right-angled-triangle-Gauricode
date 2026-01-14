5884af1db26545af63f4335d089e4547e8791dd4
unknown <gaurijkjay@gmail.com>
2026-01-14 22:44:46 +0530

    triangle code added

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,10 @@
 public class Triangle {
-    //Add your programs here
+    public static void main(){
+	for(int i=0;i<3;i++){
+		for(int j=0;j<2*i+1;j++){
+			System.out.print("* ");
+		}
+	System.out.println(" ");
+	}
+    }
 }
