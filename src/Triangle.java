82998dec1030c11eaecc9a9fd51992721bbe0a30
unknown <gaurijkjay@gmail.com>
2026-01-14 22:55:22 +0530

    error

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,10 +1,11 @@
 public class Triangle {
-    public static void main(String args[]){
-	for(int i=0;i<3;i++){
-		for(int j=0;j<2*i+1;j++){
-			System.out.print("* ");
-		}
-	System.out.println(" ");
-	}
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; i++) {                   
+		for (int j = 0; j < 2 * i + 1; j++) {  // stars: 1, 3, 5
+                System.out.print("* ");
+            }
+            System.out.println();           
+        }
     }
 }
