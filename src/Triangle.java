67934b957be35673968777be127bdcdd78a5e4af
unknown <gaurijkjay@gmail.com>
2026-01-14 22:57:39 +0530

    final

--- src/Triangle.java
+++ src/Triangle.java
@@ -3,7 +3,7 @@ public class Triangle {
 
         for (int i = 0; i < 3; i++) {                   
 		for (int j = 0; j < 2 * i + 1; j++) {  // stars: 1, 3, 5
-                System.out.print("* ");
+                System.out.print("*");
             }
             System.out.println();           
         }
