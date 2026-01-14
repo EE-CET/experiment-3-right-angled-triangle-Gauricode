6e4d69c2851e368221f9f14de1f815d8e5a00449
unknown <gaurijkjay@gmail.com>
2026-01-14 23:00:45 +0530

    err

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,11 +1,14 @@
 public class Triangle {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 3; i++) {                   
-		for (int j = 0; j < 2 * i + 1; j++) {  // stars: 1, 3, 5
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2 * i + 1; j++) {
                 System.out.print("*");
+                if (j < 2 * i) {
+                    System.out.print(" ");
+                }
             }
-            System.out.println();           
+            System.out.println();
         }
     }
 }
